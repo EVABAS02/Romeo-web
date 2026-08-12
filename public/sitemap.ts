dcf6b249434ea498782ec502c9d3ab557096dda9
EVABAS02 <leonevabas@gmail.com>
2026-08-12 14:28:16 +0100

    mettre des jour du lien vercel

--- public/sitemap.ts
+++ public/sitemap.ts
@@ -3,43 +3,43 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://ton-domaine.vercel.app',
+      url: 'https://romeo-web-gules.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://ton-domaine.vercel.app/#a-propos',
+      url: 'https://romeo-web-gules.vercel.app/#a-propos',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://ton-domaine.vercel.app/#parcours',
+      url: 'https://romeo-web-gules.vercel.app/#parcours',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://ton-domaine.vercel.app/#enseignement',
+      url: 'https://romeo-web-gules.vercel.app/#enseignement',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://ton-domaine.vercel.app/#competences',
+      url: 'https://romeo-web-gules.vercel.app/#competences',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
-      url: 'https://ton-domaine.vercel.app/#cours-epreuves',
+      url: 'https://romeo-web-gules.vercel.app/#cours-epreuves',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
-      url: 'https://ton-domaine.vercel.app/#contact',
+      url: 'https://romeo-web-gules.vercel.app/#contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.6,
